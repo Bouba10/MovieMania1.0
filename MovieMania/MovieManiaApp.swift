6f49f87f1c558ebc61dc99032503be4050302186
Bouba10 <72755006+Bouba10@users.noreply.github.com>
2024-02-13 01:03:46 +0100

    Initial Commit

---
+++ MovieMania/MovieManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieManiaApp.swift
+//  MovieMania
+//
+//  Created by Boubacar sidiki barry on 13.02.24.
+//
+
+import SwiftUI
+
+@main
+struct MovieManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
